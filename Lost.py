152c67d9f0f5b13ef1e841fff6b15d43acc0a14c
Lost Venom <149289075+LostVenom@users.noreply.github.com>
2024-04-08 11:58:53 -0700

    Update Lost.py

--- Lost.py
+++ Lost.py
@@ -9,7 +9,7 @@ from pyrogram.errors import FloodWait
 
 
 app = Client(
-    name = "Lost",
+    name = "TitanNetwrk",
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
     session_string = os.environ["SESSION_STRING"]
