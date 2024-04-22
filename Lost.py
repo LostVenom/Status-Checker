55a7f3168bc002ea3a9434d89c827d16e9171ecb
Lost Venom <149289075+LostVenom@users.noreply.github.com>
2024-04-22 01:27:59 -0700

    Made Code More Simpler by including some texts

--- Lost.py
+++ Lost.py
@@ -16,12 +16,11 @@ app = Client(
 )
 
 TIME_ZONE = os.environ["TIME_ZONE"]
-BOT_LIST = [i.strip() for i in os.environ.get("BOT_LIST").split(' ')]
-CHANNEL_ID = int(os.environ["CHANNEL_ID"]) #CHANNEL_ID is for group/channel where checker will update the status.
-MESSAGE_ID = int(os.environ["MESSAGE_ID"])
+BOT_LIST = [i.strip() for i in os.environ.get("BOT_LIST").split(' ')] #List of Bots.
+CHANNEL_ID = int(os.environ["CHANNEL_ID"]) #For Updating Bot Status.
+MESSAGE_ID = int(os.environ["MESSAGE_ID"]) #For Message which ig going to be Edited.
 BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
-GRP_ID = os.environ.get("GRP_ID") #GRP_ID is for logs group where checker will send warnings of offline bots.
-
+GRP_ID = os.environ.get("GRP_ID") #For Logs.
 async def main_Lost():
     async with app:
             while True:
